6002923851e3a72c71c7ee0c585301277d15d044
Amee Karelia <Amee.karelia16@gmail.com>
2022-04-05 16:07:27 -0700

    great number game

---
+++ Great_number_game/server.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, session, redirect, request
+import random
+from env import KEY
+
+app= Flask(__name__)
+app.secret_key= KEY
+
+@app.route('/')
+def index():
+    if 'attempts' not in session:
+        session['attempts'] = 0
+    if 'random' not in session:
+        session['random'] = random.randint(1, 100) 
+        print(session['random'])
+    return render_template("index.html" )
+
+@app.route('/process', methods=['POST'])
+def process():
+    session['guess'] = int(request.form['guess'])
+    session['attempts'] += 1
+    print(session)
+    return redirect("/")
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
